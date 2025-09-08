62c14a21e079f73c9f0a1a461d06f94c24b0c786
RonaldYou <ronaldrenyou@gmail.com>
2025-09-08 17:07:01 +0200

    Improved best practices

--- src/Vehicle.cpp
+++ src/Vehicle.cpp
@@ -1,10 +1,35 @@
 #include "Vehicle.h"
+#include "Constants.h"
+#include <stdexcept>
+#include <algorithm>
 
-// Constructor implementation
-Vehicle::Vehicle(double mass, double dragCoef, double frontalArea, 
-                 double rollingResistanceCoef, double maxEngineForce)
-    : speed(0.0), mass(mass), dragCoef(dragCoef), frontalArea(frontalArea),
-      rollingResistanceCoef(rollingResistanceCoef), maxEngineForce(maxEngineForce)) {
+Vehicle::Vehicle(double mass, 
+                 double dragCoef, 
+                 double frontalArea, 
+                 double rollingResistanceCoef, 
+                 double maxEngineForce)
+               : speed_(0.0), 
+                 mass_(mass), 
+                 dragCoef_(dragCoef), 
+                 frontalArea_(frontalArea),
+                 rollingResistanceCoef_(rollingResistanceCoef), 
+                 maxEngineForce_(maxEngineForce) {
+
+    if (mass_ <= 0) {
+        throw std::invalid_argument("Mass must be positive");
+    }
+    if (dragCoef_ < 0) {
+        throw std::invalid_argument("Drag coefficient must be non-negative");
+    }
+    if (frontalArea_ <= 0) {
+        throw std::invalid_argument("Frontal area must be positive");
+    }
+    if (rollingResistanceCoef_ < 0) {
+        throw std::invalid_argument("Rolling resistance coefficient must be non-negative");
+    }
+    if (maxEngineForce_ < 0) {
+        throw std::invalid_argument("Max engine force must be non-negative");
+    }
 }
 
 void Vehicle::update(double throttle, double dt) {
@@ -13,5 +38,5 @@ void Vehicle::update(double throttle, double dt) {
     double F_roll = rollingResistanceCoef * mass * PhysicsConstants::GRAVITY;
     double F_net = F_engine - F_drag - F_roll;
     double acceleration = F_net / mass;
-    speed += acceleration * dt;
+    speed = std::max(0.0, speed + acceleration * dt);
 }
--- src/Vehicle.h
+++ src/Vehicle.h
@@ -1,14 +1,30 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
 
 class Vehicle{
-    protected:
-        double speed;
-        double mass;
-        double dragCoef;
-        double frontalArea;
-        double rollingResistanceCoef;
-        double maxEngineForce;
+    private:
+        double speed_;
+        const double mass_;
+        const double dragCoef_;
+        const double frontalArea_;
+        const double rollingResistanceCoef_;
+        const double maxEngineForce_;
+
     public:
-        Vehicle(double mass, double dragCoef, double frontalArea,
-                double rollingResistanceCoef, double maxEngineForce);
+        explicit Vehicle(double mass, 
+                double dragCoef, 
+                double frontalArea,
+                double rollingResistanceCoef, 
+                double maxEngineForce);
+
+        [[nodiscard]] double getSpeed() const noexcept { return speed_; }
+        [[nodiscard]] double getMass() const noexcept { return mass_; }
+        [[nodiscard]] double getDragCoef() const noexcept { return dragCoef_; }
+        [[nodiscard]] double getFrontalArea() const noexcept { return frontalArea_; }
+        [[nodiscard]] double getRollingResistanceCoef() const noexcept { return rollingResistanceCoef_; }
+        [[nodiscard]] double getMaxEngineForce() const noexcept { return maxEngineForce_; }
+
         void update(double throttle, double dt);
 };
+
+#endif // VEHICLE_H
